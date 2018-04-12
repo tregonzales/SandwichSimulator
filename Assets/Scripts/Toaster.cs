5247d0f01c91f47ef43318d772be2cb5a37fd46f
jeremymoss <jmoss@macalester.edu>
2018-04-12 18:52:36 +0200

    Added Toaster Functionality

---
+++ Assets/Scripts/Toaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toaster : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Debug.Log("Enter");
+            other.gameObject.GetComponent<Rigidbody>().velocity = (Vector3.up * 20f);
+        }
+    }
+}
