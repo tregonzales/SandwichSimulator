1cc336a2220e8be1ab53b6dc7bf813da02707240
jeremymoss <jmoss@macalester.edu>
2018-04-13 16:26:39 +0200

    Table, chairs, toaster, fan, shit

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rb;
+    public int speed;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        rb.angularVelocity = Vector3.up * speed;
+    }
+	
+}
