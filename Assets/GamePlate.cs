11ebeacb3b1852ca269cb919f31ca8d9463b8550
Tre Gonzales <Tre@education.com>
2018-04-26 16:31:48 +0200

    plate finished

--- Assets/GamePlate.cs
+++ Assets/GamePlate.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlate : MonoBehaviour {
 
 	Vector3 bounce;
+	public Text counter;
 	private Color actualColor;
+	private List<GameObject> itemsColected;
+	int count;
 
 	// Use this for initialization
 	void Start () {
 		actualColor = gameObject.GetComponentInParent<Renderer>().material.color;
+		count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		gameObject.GetComponentInParent<Renderer>().material.color = Color.Lerp(Color.yellow, actualColor, Mathf.PingPong(Time.time, 2));
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!itemsColected.Contains(other.gameObject)) {
+			itemsColected.Add(other.gameObject);
+		}
+		count++;
+		counter.text = count.ToString();
+	}
+	
+	void OnTriggerExit(Collider other) {
+		itemsColected.Remove(other.gameObject);
+		count--;
+		counter.text = count.ToString();
+		if (itemsColected.Contains(other.gameObject)) {
+			itemsColected.Remove(other.gameObject);
+		}
 	}
 }
