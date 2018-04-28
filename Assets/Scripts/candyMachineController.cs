84eaa2fbd1160286495e65c6bfdedc575583806b
Tre Gonzales <Tre@education.com>
2018-04-28 20:21:20 +0200

    candy machine

---
+++ Assets/Scripts/candyMachineController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candyMachineController : MonoBehaviour {
+
+	public Transform spawnPoint;
+	public GameObject prefab;
+	Vector3 dispense;
+	public int force;
+	private Color actualColor;
+	Color randomColor;
+	GameObject instantiated;
+	private GameObject lastCollider;
+
+	// Use this for initialization
+	void Start () {
+		actualColor = gameObject.GetComponentInChildren<Renderer>().material.color;
+		dispense = transform.up + (-1*transform.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponentInChildren<Renderer>().material.color = Color.Lerp(Color.yellow, actualColor, Mathf.PingPong(Time.time, 2));
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (lastCollider == null) {
+			lastCollider = other.GetComponentInParent<Rigidbody>().gameObject;
+			instantiated = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+			randomColor = Random.ColorHSV();
+			randomColor.a = 1f;
+			instantiated.GetComponent<Renderer>().material.color = randomColor;
+			instantiated.GetComponent<Rigidbody>().velocity += (dispense * force);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.GetComponentInParent<Rigidbody>().gameObject == lastCollider) {
+			lastCollider = null;
+		}
+	}
+}
