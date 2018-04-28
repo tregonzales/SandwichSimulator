6d4e200025308593b49c72ec4cd6140f98e69243
Tre Gonzales <Tre@education.com>
2018-04-28 15:57:18 +0200

    couch and remote prefab added

---
+++ Assets/Scripts/remoteController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class remoteController : MonoBehaviour {
+
+	public Transform tvTransform;
+	public int force;
+	private Color actualColor;
+	
+	// Use this for initialization
+	void Start () {
+		actualColor = gameObject.GetComponentInChildren<Renderer>().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponentInChildren<Renderer>().material.color = Color.Lerp(Color.yellow, actualColor, Mathf.PingPong(Time.time, 2));
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponentInParent<Rigidbody>() != null) {
+			other.GetComponentInParent<Rigidbody>().velocity +=  
+			transform.TransformDirection(tvTransform.position - transform.position).normalized * force;
+		}
+	}
+}
