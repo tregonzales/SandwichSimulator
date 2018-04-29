d784a25aa8bc837915bcbbf9a1f550fee8cf66ee
Brooke Werner <brookewerner@gmail.com>
2018-04-29 14:40:24 +0200

    updates to cam controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,11 +10,12 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	//determines how the camera should move based on the current state of the game (switching items or following one item)
 	public enum CamState
 	{
 		Follow, Switch
 	}
-
+		
 	private CamState state = CamState.Follow;
 
     public Transform target;
@@ -41,27 +42,32 @@ public class CameraController : MonoBehaviour {
         angleY = 180;
 	}
 
+	//when switching from one item to another
 	public void SwitchTarget(Transform newTarget){
-		target = newTarget;
-		state = CamState.Switch;
-		if (switchRoutine != null) {
+		target = newTarget; //change targets to the new item
+		state = CamState.Switch; //change state of camera to switching
+		if (switchRoutine != null) { //if the camera was in the middle of switching items, disrupt it
 			StopCoroutine (switchRoutine);
 		}
-		switchRoutine = StartCoroutine(lerpSwitch(1f));
+		switchRoutine = StartCoroutine(lerpSwitch(1f)); //begin coroutine to switch to the new item, and store it to switchRoutine variable to keep track of whether the camera is currently running a coroutine or not
 	}
 
 	void Update () {
+
+		//using input from the player on xbox controller, determine where to move the camera
 		angleX += gameInputManager.getStick ("RightStickY") * Time.deltaTime * verticalSpeed * -1;
 		angleY += gameInputManager.getStick ("RightStickX") * Time.deltaTime * verticalSpeed * -1;
 
+		//using input from keyboard, determine where to move the camera
 		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
 			angleX += Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed * -1;
 			angleY += Input.GetAxis("Horizontal") * Time.deltaTime * verticalSpeed * -1;
 		}
-
+			
 		angleX = Mathf.Clamp (angleX, minVerticalAngle, maxVerticalAngle);
 		angleY %= 360;
 
+		//calculate rotation of camera
 		Quaternion xRotation = Quaternion.AngleAxis (angleX, new Vector3 (1, 0, 0));
 		Quaternion yRotation = Quaternion.AngleAxis (angleY, new Vector3 (0, 1, 0));
 		offset = new Vector3 (0, 0, 1);
@@ -69,18 +75,23 @@ public class CameraController : MonoBehaviour {
 		offset = yRotation * offset;
 		offset *= distance;
 
+		//offset here becomes the result of offset plus the target position and target to camera
+		//otherwise becomes the hit point if it has to avoid an obstacle
 		offset = AddObstacleAvoidance (offset);
 			
 		switch (state) {
 		case CamState.Follow:
+			//if camera state is following, then set the new position and new rotation
 			transform.position = offset;
 			transform.rotation = Quaternion.LookRotation (target.position - transform.position, new Vector3 (0, 1, 0));
 			break;
 		case CamState.Switch:
+			//if camera state is switch, do nothing here because the SwitchTarget method will be called instead
 			break;
 		}
 	}
-
+		
+	//use lerp to move from one item to another for a smooth linear transition
 	public IEnumerator lerpSwitch(float duration) {
 		float currentTime = 0;
 		float endTime = duration;
@@ -89,12 +100,17 @@ public class CameraController : MonoBehaviour {
 		while (true) {
 			currentTime += Time.deltaTime;
 			float normalizedTime = currentTime / endTime;
+
+			//ease the amount of time with an easing function with normalized time
 			float easedTime = EasingFunction.EaseOutCubic (0, 1, normalizedTime);
 
-			transform.position = Vector3.Lerp(startPos, offset, easedTime); //changed to Lerp instead of Slerp
+			//lerp to the new position and new rotation from the old position/rotation
+			transform.position = Vector3.Lerp(startPos, offset, easedTime);
 			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position, new Vector3 (0, 1, 0)), easedTime);
 
 			yield return null;
+
+			//once the set duration time is up, end loop
 			if (currentTime > endTime) {
 				break;
 			}
@@ -109,6 +125,7 @@ public class CameraController : MonoBehaviour {
 			// if we hit an object between camera and target position the camera at (in front of) the hit object.
 			return hit.point;
 		}
+		//if we don't hit an object, then the new position will be returned based off target position and target to camera
 		return target.position + targetToCamera;
 	}
 }
